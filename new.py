4219c57cbc308c9a69897f470077945f9890904f
vijaychandra800 <vijaychandra800@gmail.com>
2021-05-29 22:58:29 +0530

    Click Table Of Contents

--- new.py
+++ new.py
@@ -2,8 +2,12 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(r"D:\VSCode\WordPress_Automation\chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(chrome_options=options, executable_path=r'I:\chromedriver.exe')
 driver.maximize_window()
 driver.get("https://crackplex.com/wp-admin/edit.php")
 
@@ -11,8 +15,20 @@ removeImage="document.getElementsByTagName(\"img\")[0].remove()"
 beforeH2 = 'document.getElementsByTagName("h2")[0].previousElementSibling.remove()'
 
 
-def returnTitle():
-    pass
+def removeLast():
+    last = driver.find_elements_by_tag_name("strong")
+    for i in range(1,5):
+        last[-i].clear()
+
+def checkBox():
+    driver.switch_to.default_content()
+    actions = ActionChains(driver)
+    main_box = driver.find_elements_by_id('ez-toc-settings[insert-toc]')[0]
+    actions.click(main_box)
+    actions.perform()
+    for i in range(1,7):
+        driver.find_element_by_id('ez-toc-settings[heading-levels]['+str(i)+']').click()
+
 
 driver.find_element_by_name("log").send_keys("CrackPlex")
 time.sleep(3)
@@ -25,9 +41,9 @@ time.sleep(7)
 driver.switch_to.frame('content_ifr')
 driver.execute_script(removeImage)
 driver.execute_script(beforeH2)
-time.sleep(10)
-mirror = driver.find_elements_by_partial_link_text("Mirror")
-#for i in range(0,2):
- #   driver.find_elements_by_partial_link_text("Mirror")[i].clear()
-
+time.sleep(5)
+removeLast()
+time.sleep(7)
+checkBox()
 time.sleep(10)    
+
