088d2912ffa28ac1e0cf633a3ef7c32362bb6900
vijaychandra800 <vijaychandra800@gmail.com>
2021-05-29 13:12:10 +0530

    Open First Post

--- new.py
+++ new.py
@@ -3,22 +3,27 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-post1="//*[@id=\"post-530\"]/td[1]/strong/a"
-del_img="//*[@id=\"tinymce\"]/div/div/p[1]"
-
-driver = webdriver.Chrome(r"C:\Users\vijay\Downloads\selenium\chromedriver.exe")
+driver = webdriver.Chrome(r"I:\Code\WordPress_Automation\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://crackplex.com/wp-admin/edit.php")
+
+removeImage="document.querySelector(\"#tinymce > div:nth-child(2) > div > p:nth-child(1)\").remove()"
+youAlso="document.querySelector(\"#tinymce > div > div > p:nth-child(2)\").remove()"
+
+def returnTitle():
+    pass
+
 driver.find_element_by_name("log").send_keys("CrackPlex")
 time.sleep(3)
 driver.find_element_by_name("pwd").send_keys("5sUnf8pq$YAryr$1oc")
 time.sleep(3)
 driver.find_element_by_name("wp-submit").send_keys(Keys.ENTER)
 time.sleep(5)
-driver.find_element_by_xpath(post1).click()
-#driver.find_element_by_name("post_title").send_keys(Keys.BACKSPACE)
+driver.find_element_by_class_name('row-title').click()
 time.sleep(7)
-driver.find_element_by_xpath(del_img).send_keys(Keys.DELETE)
+driver.switch_to.frame('content_ifr')
+driver.execute_script(removeImage)
+driver.execute_script(youAlso)
 time.sleep(10)
 
-#driver.close()  
+#driver.find_elements_by_partial_link_text("Mirror")
