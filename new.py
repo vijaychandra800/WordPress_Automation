bee23041b6e785f6e63ce9bcf027e7cd22900e75
Vijay Chandra <vijaychandra800@gmail.com>
2021-05-29 15:56:07 +0530

    Fixed Delete Image, Delete Text above H2

--- new.py
+++ new.py
@@ -3,12 +3,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(r"I:\Code\WordPress_Automation\chromedriver.exe")
+driver = webdriver.Chrome(r"D:\VSCode\WordPress_Automation\chromedriver.exe")
 driver.maximize_window()
 driver.get("https://crackplex.com/wp-admin/edit.php")
 
-removeImage="document.querySelector(\"#tinymce > div:nth-child(2) > div > p:nth-child(1)\").remove()"
-youAlso="document.querySelector(\"#tinymce > div > div > p:nth-child(2)\").remove()"
+removeImage="document.getElementsByTagName(\"img\")[0].remove()"
+beforeH2 = 'document.getElementsByTagName("h2")[0].previousElementSibling.remove()'
+
 
 def returnTitle():
     pass
@@ -23,7 +24,10 @@ driver.find_element_by_class_name('row-title').click()
 time.sleep(7)
 driver.switch_to.frame('content_ifr')
 driver.execute_script(removeImage)
-driver.execute_script(youAlso)
+driver.execute_script(beforeH2)
 time.sleep(10)
+mirror = driver.find_elements_by_partial_link_text("Mirror")
+#for i in range(0,2):
+ #   driver.find_elements_by_partial_link_text("Mirror")[i].clear()
 
-#driver.find_elements_by_partial_link_text("Mirror")
+time.sleep(10)    
