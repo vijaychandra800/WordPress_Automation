d81be0cdcfbc2462bccde9f26c80bbbf8c4b9ebd
vijaychandra800 <vijaychandra800@gmail.com>
2021-06-03 10:55:01 +0530

    Final Changes

--- new.py
+++ new.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(chrome_options=options, executable_path=r'I:\chromedriver.exe')
+driver = webdriver.Chrome(chrome_options=options)
 driver.maximize_window()
 driver.get("https://crackplex.com/wp-admin/edit.php")
 
@@ -16,8 +16,9 @@ beforeH2 = 'document.getElementsByTagName("h2")[0].previousElementSibling.remove
 
 def removeLast():
     last = driver.find_elements_by_tag_name("strong")
-    for i in range(1,5):
+    for i in range(1,len(last)):
         last[-i].clear()
+        
 
 def checkBox():
     driver.switch_to.default_content()
@@ -28,9 +29,9 @@ def checkBox():
     for i in range(1,7):
         driver.find_element_by_id('ez-toc-settings[heading-levels]['+str(i)+']').click()
 
-def metaDescription():
+def metaDescription(key):
     default = "Soft Crack, Download Soft Crack For Free. Activate Soft For Free. How to Crack Soft 2021. Soft Free Download."
-    new = default.replace("Soft", download.key)
+    new = default.replace("Soft", key)
     
     #Change from Post Iframe to Default
     driver.switch_to.default_content()
@@ -46,27 +47,39 @@ def metaDescription():
 
 def download(key):
     remove = 'document.getElementsByTagName("h4")[0].nextElementSibling.remove()'
-    align_left = 'document.getElementsByTagName("h4")[0].style.textAlign = "left"'
+    align_left = 'document.getElementsByTagName(\'h4\')[0].style.textAlign = \'left\';'
+    if(bool(driver.find_elements_by_tag_name('h4'))==True):
+        driver.execute_script(remove, align_left)
+    else:
+        find_length = len(driver.find_elements_by_tag_name('h3'))
+        remove = 'document.getElementsByTagName("h3")['+str(find_length-1)+'].nextElementSibling.remove()'
+        align_left = 'document.getElementsByTagName(\'h3\')['+str(find_length-1)+'].style.textAlign = \'left\';'
+        print(remove, align_left)
+        driver.execute_script(remove, align_left)    
     driver.switch_to.default_content()
     driver.find_element_by_id('content-html').click()
-    download = """<h4>How To Crack Tally</h4>
-<ol>
- 	<li>Download <a href="https://winfreewares.xyz/full-setup-download/"><strong>Tally Crack</strong></a> from the below links.</li>
- 	<li>Uninstall any previous version of Softwares using <a href="https://crackplex.com/revo-uninstaller-pro-crack-latest-version-direct-link/">Revo Uninstaller Pro Crack</a>.</li>
- 	<li>Install the software but don't run it.</li>
- 	<li>Now Run the Patch file.</li>
- 	<li>You've Successfully cracked Tally Pro for Free, Enjoy a Full Version of the software for Free.</li>
-</ol>
-<h4>Tally Crack Direct Download Links</h4>"""
+    time.sleep(7)
+    download = ("""<h4>How To Crack Tally</h4><ol><li>Download <a href="https://winfreewares.xyz/full-setup-download/"><strong>Tally Crack</strong></a> from the below links.</li><li>Uninstall any previous version of Softwares using <a href="https://crackplex.com/revo-uninstaller-pro-crack-latest-version-direct-link/">Revo Uninstaller Pro Crack</a>.</li><li>Install the software but don't run it.</li><li>Now Run the Patch file.</li><li>You've Successfully cracked Tally Pro for Free, Enjoy a Full Version of the software for Free.</li></ol><h4>Tally Crack Direct Download Links</h4>""")
     new_download = download.replace("Tally", key)
 
+    #Sleep
+    time.sleep(3)
+    
     #Go to new Line and enter Text
-    driver.find_element_by_tag_name('h4').send_keys(Keys.ENTER, new_download)
+    driver.switch_to.frame('content_ifr')
+    a = bool(driver.find_elements_by_tag_name('h4'))
+    if(a == True):
+        driver.find_elements_by_tag_name('h4')[-1].send_keys(new_download)
+    elif(a == False):
+        #h3_length = driver.find_elements_by_tag_name('h3')
+        #driver.find_elements_by_tag_name('h3')[-1].send_keys(Keys.ENTER)
+        driver.find_elements_by_tag_name('h3')[-1].send_keys(new_download)
 
     #Return to Visual
     #driver.switch_to.default_content()
+    time.sleep(7)
+    driver.switch_to.default_content()
     driver.find_element_by_id('content-tmce').click()
-
 
 driver.find_element_by_name("log").send_keys("CrackPlex")
 time.sleep(3)
@@ -82,11 +95,9 @@ driver.execute_script(beforeH2)
 time.sleep(5)
 removeLast()
 time.sleep(7)
-download('eTally Pro')
+key = 'eTally Pro'
+download(key)
 time.sleep(7)
-metaDescription()
-time.sleep(7)
+metaDescription(key)
 checkBox()
 time.sleep(20)
-
-
